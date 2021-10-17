07892f788eccddbfe9cfaaefedadde98562baf08
davidwpacker <davidwgpacker@gmail.com>
2021-10-17 12:41:01 +1100

    initial file structure + CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1> *testing github pages w/ react and github actions</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <h1> *testing github pages w/ react and github actions</h1>
+          </p>
+           
+          
+        </header>
+      </div>
+    </Router>
   );
 }
 
