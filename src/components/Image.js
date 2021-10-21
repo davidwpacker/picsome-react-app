ffaa31715d479e90c3be1713bbfad83e04de3c7e
davidwpacker <davidwgpacker@gmail.com>
2021-10-21 14:45:40 +1100

    image.js

---
+++ src/components/Image.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react"
+
+function Image({className, img}) {
+    const [hovered, setHovered] = useState(false)
+    
+    return (
+        <div 
+            className={`${className} image-container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <img src={img.url} className="image-grid"/>
+        </div>
+    )
+}
+
+export default Image
